e14352afa25481a9aa2521f6423d41f1b9d7d58b
bje0716 <byon0716@gmail.com>
2020-07-24 16:02:43 +0900

    modified util
    Signed-off-by: bje0716 <byon0716@gmail.com>

--- app/src/main/java/com/grapefruit/schoolmeal/util/Util.java
+++ app/src/main/java/com/grapefruit/schoolmeal/util/Util.java
@@ -1,4 +1,42 @@
 package com.grapefruit.schoolmeal.util;
 
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import androidx.core.content.ContextCompat;
+
+import com.grapefruit.schoolmeal.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 public class Util {
+
+    public static boolean isNetwork(Context context) {
+        ConnectivityManager conn = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = conn.getActiveNetworkInfo();
+        return info != null && info.isConnected();
+    }
+
+    public static Drawable setBackArrowColor(Context context) {
+        Drawable upArrow = ContextCompat.getDrawable(context, R.drawable.abc_ic_ab_back_material);
+        upArrow.setColorFilter(ContextCompat.getColor(context, R.color.black), PorterDuff.Mode.SRC_ATOP);
+        return upArrow;
+    }
+
+    public static String getToday(long time) {
+        return new SimpleDateFormat("yyyy년 MM월 dd일 a hh:ss", Locale.getDefault()).format(time);
+    }
+
+    public static int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
 }
